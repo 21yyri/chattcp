a33ded95e0007dd1372566e9d708715b4e161f43
Yurii <155418226+21yyri@users.noreply.github.com>
2025-05-26 08:27:20 -0300

    Add files via upload

---
+++ cliente.py
@@ -0,0 +1,29 @@
+import socket
+from threading import Thread
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cliente.connect(('localhost', 9999))
+
+username = input('Insira seu nome de usuário: ').strip().lower()
+
+def sendMessage():
+    while True:
+        msg = input()
+        if msg == 'quit':
+            cliente.close()
+            break
+
+        msg = f'{username}: {msg}'
+        cliente.send(msg.encode())
+
+# def recvMessage():
+#     while True:
+#         server_msg = cliente.recv(1024).decode()
+#         print(server_msg)
+
+# threadRecv = Thread(target=recvMessage)
+# threadRecv.start()
+
+threadSend = Thread(target=sendMessage)
+threadSend.start()
+threadSend.join()
